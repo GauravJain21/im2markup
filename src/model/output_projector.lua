19d3cb640280e908080d6e42e6a0f3ea8cd208a8
Gaurav Jain <10961005+GauravJain21@users.noreply.github.com>
2018-04-22 19:24:11 +0530

    Update output_projector.lua

--- src/model/output_projector.lua
+++ src/model/output_projector.lua
@@ -4,5 +4,10 @@ function createOutputUnit(input_size, output_size)
     local model = nn.Sequential()
     model:add(nn.Linear(input_size, output_size))
     model:add(nn.LogSoftMax())
+    --[[This model takes input_size input, linearly 
+		transforms it into output_size output using
+		y = A * x + b, and finally applies log of 
+		Softmax to this output
+		]]
     return model
 end
