401ff6d661a20758672db89ddf8696eefa87c059
Gaurav Jain <10961005+GauravJain21@users.noreply.github.com>
2018-04-22 19:29:51 +0530

    Update criterion.lua

--- src/model/criterion.lua
+++ src/model/criterion.lua
@@ -7,6 +7,21 @@ function createCriterion(output_size)
     --    weights[i] = 16
     --end
     criterion = nn.ClassNLLCriterion(weights)
+    --[[NLL = negative log likelihood, weights are assigned
+    	to classes because the training set is unbalanced.
+
+    	Due to the behaviour of the backend code,
+    	it is necessary to set sizeAverage to false 
+    	when calculating losses in non-batch mode.
+    	
+    	Indeed, the ignoreIndex (defaults to -100) 
+    	specifies a value for targets to be ignored. 
+    	The commensurate gradInput for that target will 
+    	be zero. When sizeAverage=true (the default), 
+    	the gradInput and output are averaged over 
+    	non-ignored targets.
+    ]]
     criterion.sizeAverage = false
+
     return criterion
 end
